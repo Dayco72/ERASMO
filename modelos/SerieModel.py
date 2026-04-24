9a6653dce67bc75d06a5ae70f606f6df2cf68293
Dayco72 <139602627+Dayco72@users.noreply.github.com>
2026-04-23 19:25:59 -0600

    Delete modelos directory

--- modelos/SerieModel.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import Field, SQLModel
-from datetime import date
-
-class SerieModel(SQLModel, table = True):
-    __tablename__ = "serie"
-    ser_id: int | None = Field(default=None, primary_key=True)
-    ser_nombre: str
-    ser_fecha_ini: date
-    ser_fecha_fin : date
